34d109b6278cd5a9d7a37518247427d856d14b9e
Prakash Adhikari <prakash.ad@gmail.com>
2020-09-12 19:14:22 -0400

    removed stripe related routes to organize code

--- routes/orders.js
+++ routes/orders.js
@@ -9,12 +9,6 @@ const {
   ensureCorrectUserOrder, 
   ensureCorrectUser
 } = require("../middleware/auth");
-
-//for stripe payment
-const { STRIPE_SECRET_KEY } = require("../secret");
-const stripe = require("stripe")(STRIPE_SECRET_KEY);
-const bodyParser = require('body-parser');
-
 const router = express.Router();
 
 /** GET/ => { orders: [order, ...]} */
@@ -26,16 +20,6 @@ router.get("/", adminRequired, async function(req, res, next){
     return next(err);
   }
 });
-
-// /** GET/[name] => {order: order} */
-// router.get("/:id", authRequired, ensureCorrectUserOrder, async function(req, res, next){
-//   try {
-//     const order = await Order.findOne(req.params.id);
-//     return res.json({ order });
-//   } catch (err){
-//     return next(err);
-//   }
-// });
 
 /** GET/[name] => {order: order} */
 router.get("/:username", ensureCorrectUser, async function(req, res, next){
@@ -89,90 +73,4 @@ router.post("/shipping", ensureCorrectUserOrder, async function(req, res, next){
   }
 })
 
-router.post('/webhook', 
-  bodyParser.raw({type: 'application/json'}),
-  async function (request, response) {
-    let event;
-
-    try {
-      event = JSON.parse(request.body);
-    } catch (err) {
-      response.status(400).send(`Webhook Error: ${err.message}`);
-    }
-
-    console.log('************ test ***********');
-    // Handle the event
-    switch (event.type) {
-      case 'payment_intent.succeeded':
-        const paymentIntent = event.data.object;
-      
-        // Then define and call a method to handle the successful payment intent.
-        // handlePaymentIntentSucceeded(paymentIntent);
-        // await Order.addPayment( orderId, paymentIntent); 
-        console.log('before switch !!!!!!!!!!!', paymentIntent);
-
-        break;
-      case 'payment_method.attached':
-        const paymentMethod = event.data.object;
-        // Then define and call a method to handle the successful attachment of a PaymentMethod.
-        // handlePaymentMethodAttached(paymentMethod);
-        break;
-      // ... handle other event types
-      default:
-        // Unexpected event type
-        return response.status(400).end();
-    }
-  // Return a response to acknowledge receipt of the event
-  response.json({received: true});
-});
-
-
-/** POST /{payment}  => {payment : payment }*/
-router.post("/payment", ensureCorrectUserOrder, async function(req, res, next){
-  try {
-    const orderId = req.body.order_id;
-    const customer = await stripe.customers.create();
-    const { total_price } = await Order.totalAmount( orderId );
-    const totalPrice = 100 * Number.parseFloat(total_price);
-
-    // Create a PaymentIntent with the order amount and currency
-    const paymentIntent = await stripe.paymentIntents.create({
-      customer: customer.id,
-      setup_future_usage: 'off_session',
-      amount: totalPrice,
-      currency: "usd"
-    });
-    res.json({
-      clientSecret: paymentIntent.client_secret
-    });
-    console.log("---------->>>>> paymentIntent : ", paymentIntent);
-    // await Order.addPayment( orderId, paymentIntent.client_secret); 
-  } catch(err) {
-    return next(err);
-  }
-})
-
-
 module.exports = router;
-
-
-
-
-
-
-
-  /** just following would post payement intent */
-  // try {
-  //   const orderId = req.body.order_id;
-  //   const { total_price } = await Order.totalAmount( orderId );
-  //   const totalPrice = 100 * Number.parseFloat(total_price);
-  //   const paymentIntent = await stripe.paymentIntents.create({
-  //     amount: totalPrice,
-  //     currency: "usd"
-  //   });
-  //   const pIntent = paymentIntent.client_secret;
-  //   res.send ({ clientSecret: pIntent });
-  //   await Order.addPayment( orderId, pIntent); 
-  // } catch(err) {
-  //   return next(err);
-  // }
